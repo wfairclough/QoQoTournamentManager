8e816d12bb8ff80d9742ca4615b1c27600296c69
Will Fairclough <wfairclough@gmail.com>
2012-05-20 15:28:28 -0400

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QoQoTournamentManager::Application.config.secret_token = '653b2be007ec16b99788fb0319accfdb2f7c61dc53c62a85f7e696e234603f52d7e241902dd05026f08dcd65169eb053877413be0d6f5eb43d58f2c7f432c8a6'
